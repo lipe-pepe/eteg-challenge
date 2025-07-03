21d385cb5948f7d49914bbfb161e206630c655b9
Felipe Pêpe <felipepepe21@gmail.com>
2025-07-03 18:59:30 -0300

    feat: :bulb: added comments in cpf formatter

--- frontend/src/utils/formatCpf.ts
+++ frontend/src/utils/formatCpf.ts
@@ -1,8 +1,8 @@
 export function formatCpf(value: string) {
   return value
-    .slice(0, 14) // limita a 14 caracteres (com pontos e hífen)
-    .replace(/\D/g, "") // remove não dígitos
-    .replace(/(\d{3})(\d)/, "$1.$2")
-    .replace(/(\d{3})(\d)/, "$1.$2")
-    .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    .slice(0, 14) // Limita a 14 caracteres (com pontos e hífen)
+    .replace(/\D/g, "") // Remove não dígitos
+    .replace(/(\d{3})(\d)/, "$1.$2") // Adiciona o terceiro "."
+    .replace(/(\d{3})(\d)/, "$1.$2") // Adiciona o segundo "."
+    .replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Adiciona o "-"
 }
